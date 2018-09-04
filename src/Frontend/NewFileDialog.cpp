5f2063333d6843f63431744082e5f087afaa21a6
fsfod <fsfod11@gmail.com>
2018-09-04 08:56:28 +0100

    Fix ambiguous call to wxGridSizer constructor in NewFileDialog

--- src/Frontend/NewFileDialog.cpp
+++ src/Frontend/NewFileDialog.cpp
@@ -35,7 +35,7 @@ NewFileDialog::NewFileDialog( wxWindow* parent, bool enableSourceControl )
     : wxDialog( parent, wxID_ANY, wxT("Add New File"), wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE | wxRESIZE_BORDER )
 {
 
-    wxGridSizer* sizer = new wxGridSizer(1, 1);
+    wxGridSizer* sizer = new wxGridSizer(1, 1, 1, 1);
 	
 	wxFlexGridSizer* fgSizer1;
 	fgSizer1 = new wxFlexGridSizer( 1, 1, 0, 0 );
