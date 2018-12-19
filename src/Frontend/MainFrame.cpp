f70b39dc1173c6131961b438c5967022e1681be1
fsfod <fsfod11@gmail.com>
2018-12-19 17:09:19 +0000

    Fix the auto completion list not being populated with data until a a file is saved
    When the background parse is complete the completion manager is now
    automatically populated with the data.

--- src/Frontend/MainFrame.cpp
+++ src/Frontend/MainFrame.cpp
@@ -6029,7 +6029,9 @@ void MainFrame::OnSymbolsParsed(SymbolParserEvent& event)
 
             //Set the project to itself so we can trigger Rebuild()
             m_projectExplorer->SetProject(m_project);
-            
+
+            m_autoCompleteManager.BuildFromProject(m_project);
+
             return;
         }
     }
